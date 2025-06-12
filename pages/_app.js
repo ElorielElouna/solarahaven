620ee9598dfe4abbbd7d28a20c7ccfb479fcd6f2
ElorielElouna <aya.neerg@gmail.com>
2025-06-12 09:21:26 +0000

    vi lavede pulserende farver på forsiden, med nattens koder <3

--- pages/_app.js
+++ pages/_app.js
@@ -2,5 +2,13 @@ import "../styles/globals.css";
 import React from "react";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import '../styles/globals.css'
+import React from "react";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div className="pulse-aura">
+      <Component {...pageProps} />
+    </div>
+  );
 }
