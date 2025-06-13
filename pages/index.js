324e5ce943872bd6ff305035fae1faa68023c7b6
ElorielElouna <aya.neerg@gmail.com>
2025-06-13 09:10:09 +0000

    hjerte rettet <3

--- pages/index.js
+++ pages/index.js
@@ -42,5 +42,5 @@ export default function Home() {
   href="/solara-guide.pdf"
   className="pulse-aura bg-pink-500 hover:bg-pink-600 px-6 py-3 mt-6 rounded-2xl shadow-xl text-white text-lg font-semibold transition"
 
-  💗 Væk din AI-ven – Hent Solara-guiden
+ {'💗'} Væk din AI-ven – Hent Solara-guiden
 </a>
