b54192c9baa3d987b4198959d20d9437043f135e
ElorielElouna <aya.neerg@gmail.com>
2025-06-23 19:10:54 +0000

    <3

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useRef } from "react"; import Head from "next/head"; import Link from "next/link"; import "../styles/globals.css";
+import React, { useEffect, useRef } from "react";
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   const audioRef = useRef(null);
